ebb04bcf5025e8c3a846077a819b79a31c68511e
pavanpodila <pavan.podila@gmail.com>
2019-03-20 23:00:41 +0530

    first checkin with enum types

---
+++ enums.ts
@@ -0,0 +1,32 @@
+enum ResultType {
+  pending,
+  completed,
+  failed,
+}
+
+enum StringResultType {
+  pending = 'Pending',
+  completed = 'Completed',
+  failed = 'Failed',
+}
+
+enum IntegerResultType {
+  pending = 0,
+  completed = 1,
+  failed = 2,
+}
+
+type StringLiteralResultType = 'pending' | 'completed' | 'failed';
+type NumberLiteralResultType = 0 | 1 | 2;
+
+interface Pending {
+  kind: 'pending';
+}
+interface Completed {
+  kind: 'completed';
+}
+interface Failed {
+  kind: 'failed';
+}
+
+type UnionResultType = Pending | Completed | Failed;
